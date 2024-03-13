14bb63ae3ee2f5f1d63eb71abddaefdb5b2b3b98
AnushreeGaikwad <anushreegaikwad27@gmail.com>
2024-03-13 06:27:51 -0400

    Q1.c

---
+++ Q1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+  int n1,n2,sum,sub,product;
+  printf("Enter the first number: ");
+  scanf("%d", &n1);
+  printf("Enter the second number: ");
+  scanf("%d", &n2);
+  sum=n1+n2;
+  sub=n1-n2;
+  product=n1*n2;
+  printf("sum: %d\n",sum);
+  printf("sub: %d\n",sub);
+  printf("Product: %d\n",product);
+  return 0;
+
+
+
+
+
+
+
+
+
+
+}
