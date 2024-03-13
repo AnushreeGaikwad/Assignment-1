7777e2e56d0fb7130276885e250988db19b11423
AnushreeGaikwad <anushreegaikwad27@gmail.com>
2024-03-13 12:28:47 -0400

    Q2

---
+++ Q2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("Enter the value of a: ");
+  scanf("%d",&a);
+  printf("Decimal value is : %d\n",a);
+  printf("Octal value is : %o\n",a);
+  printf("Hexadecimal value is : %x\n",a);
+ 
+
+
+
+
+
+
+
+return 0;
+}
