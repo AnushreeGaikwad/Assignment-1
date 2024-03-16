21a77c68464bbd693b36b18b6afe7f88d5ce4f57
AnushreeGaikwad <anushreegaikwad27@gmail.com>
2024-03-16 07:51:53 -0400

    Q9

---
+++ Q9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  float fahrenheit,celsius;
+  celsius=24;
+  fahrenheit=((celsius*9)/5)+32;
+  printf("\n\n Temperature in fahrenheit is: %f",fahrenheit);
+
+
+
+
+
+
+
+
+
+return 0;
+}
